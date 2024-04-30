56c207838d143ba1c49d490f249907d29b32e6c3
PrabhuKumaravel <plestarprabhu@gmail.com>
2024-04-30 18:38:11 +0530

    dashboard update

---
+++ emp/app/account/layout.tsx
@@ -0,0 +1,28 @@
+import React, { ReactNode} from 'react'
+import Link from "next/link"
+import { useRouter } from 'next/router';
+
+//components
+import AdminNavbar from '../components/Navbars/AdminNavbar';
+import Sidebar from '../components/Sidebar/sidebar';
+
+interface DashboardProps {
+    children: ReactNode;
+  }
+
+
+const layout: React.FC<DashboardProps> = ({children}) => {
+  return (
+    <div>
+        <div>
+            <Sidebar/>
+        </div>
+        <div>
+            <AdminNavbar/>
+            {children}
+        </div>
+    </div>
+  )
+}
+
+export default layout
