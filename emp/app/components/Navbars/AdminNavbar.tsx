9bbadfd464b66555a22e502e92e8b3f64b908c5b
PrabhuKumaravel <plestarprabhu@gmail.com>
2024-04-30 18:38:11 +0530

    dashboard update

---
+++ emp/app/components/Navbars/AdminNavbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+//import UserDropdown from "components/Dropdowns/UserDropdown";
+
+export default function Navbar() {
+  return (
+    <>
+      <div>Prabhu</div>
+    </>
+  );
+}
