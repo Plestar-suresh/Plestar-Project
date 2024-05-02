156c483cb88054837539421093a0e3cbc7ce8f86
PrabhuKumaravel <plestarprabhu@gmail.com>
2024-05-02 19:31:40 +0530

    leave page update

--- emp/app/leaves/page.tsx
+++ emp/app/leaves/page.tsx
@@ -4,29 +4,30 @@ import LeaveForm from '../components/LeaveForm/LeaveForm';
 import PermissionForm from '../components/PermissionForm/PermissionForm';
 
 const Leaves = () => {
-    const [isFormVisible, setIsFormVisible] = useState(false);
+    const [isLeaveFormVisible, setIsLeaveFormVisible] = useState(false);
+    const [isPermissionFormVisible, setIsPermissionFormVisible] = useState(false);
 
     const handleDropdownChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        event.preventDefault(); 
         const { value } = event.target;
-        // Toggle the visibility of the form based on the selected value
-        setIsFormVisible(value === 'leave' || value === 'permission');
+        
+        if (value === 'leave') {
+            setIsLeaveFormVisible(true);
+            setIsPermissionFormVisible(false);
+          } else if (value === 'permission') {
+            setIsLeaveFormVisible(false);
+            setIsPermissionFormVisible(true);
+          }
     };
   return (
     <div style={{ height: '100vh', width: '100vw' }} className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
-      <select /* onChange={handleDropdownChange} */>
+      <select onChange={handleDropdownChange}>
         <option value="">Select an option</option>
         <option value="leave">Leave Form</option>
         <option value="permission">Permission Form</option>
       </select>
-      {isFormVisible && (
-        <div>
-          {/* Your leave or permission form components go here */}
-          {/* For example: */}
-          <LeaveForm />
-          {/* Or */}
-          <PermissionForm />
-        </div>
-      )}
+      {isLeaveFormVisible && <LeaveForm />}
+      {isPermissionFormVisible && <PermissionForm />}
     </div>
   )
 }
