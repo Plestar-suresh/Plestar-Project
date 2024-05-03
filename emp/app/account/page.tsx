c722fe9292f888288526399e485271361bc70f76
PrabhuKumaravel <plestarprabhu@gmail.com>
2024-05-03 12:17:29 +0530

    leave form update

--- emp/app/account/page.tsx
+++ emp/app/account/page.tsx
@@ -6,6 +6,9 @@ const dashboard = () => {
     <div className="max-w-4xl mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-4">Dashboard</h1>
       <div className="bg-white shadow-md rounded-md p-6 mb-4">
+        <a href="/leaves">Leaves</a>
+      </div>
+      <div className="bg-white shadow-md rounded-md p-6 mb-4">
         <h2 className="text-xl font-semibold mb-2">Profile Name</h2>
         <p className="text-gray-600">John Doe</p>
       </div>
